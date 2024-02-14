c41cf89123508f1ae479d3df69497a897aae9ca8
Tom-W-Allen <tomwa1@live.co.uk>
2024-02-14 15:30:42 +0000

    Initial version of app

---
+++ CourseBookingApp/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+class Course:
+    def __init__(self, course_name, convenor, number_of_students):
+        self.course_name = course_name
+        self.convenor = convenor
+        self.number_of_students = number_of_students
+
+
+course_list = [Course("Biology", "Jim Smith", 5),
+               Course("Maths", "Jack Jones", 8)]
+
+
+@app.get("/")
+async def home():
+    return "Hello World"
+
+
+@app.get("/courses", response_class=HTMLResponse)
+async def test(request: Request):
+    return templates.TemplateResponse(request=request,
+                                      name="ViewCourses.html",
+                                      context={"course_list": course_list})
